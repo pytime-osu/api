6d139c330ca4d868b051ed610754e5beeee95b06
ashwin <ashraj98@gmail.com>
2020-10-01 12:36:15 -0400

    Added suggestion viewset with search view.

---
+++ core/views/suggestion.py
@@ -0,0 +1,14 @@
+from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
+from core.models import Suggestion
+
+
+class SuggestionViewSet(viewsets.ViewSet):
+    @action(detail=False, methods=['GET'])
+    def search(self, request):
+        suggestions = Suggestion.objects.filter(
+            name__contains=request.query_params.get('search', '')
+        )[:5].values_list('name')
+        return Response([{'label': s, 'value': s} for s in suggestions])
