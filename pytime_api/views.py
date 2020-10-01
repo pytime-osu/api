15c2cd536b25c84e9b7fd155a4de2267dc05c745
ashwin <ashraj98@gmail.com>
2020-10-01 08:39:55 -0400

    Fixed issues in view logic.

--- pytime_api/views.py
+++ pytime_api/views.py
@@ -2,7 +2,6 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from discovery import DiscoveryClient
 from django.conf import settings
-import json
 
 discovery = DiscoveryClient()
 
@@ -10,27 +9,26 @@ discovery = DiscoveryClient()
 # Create your views here.
 @api_view(['POST'])
 def rec_list(request):
-    if request.method == 'POST':
-        tags = "".join(request.data['tag'])
-        games_list = discovery.query(
-            collection_id=settings.DISCOVERY_COLLECTION_ID,
-            environment_id=settings.DISCOVERY_ENVIRONMENT_ID,
-            natural_language_query=tags)['results']
-        results = []
-        for game in games_list:
-            results.append({"name": game['name'], "summary": game['summary'], "cover_url": game['cover']['url'],
-                            "slug": game['slug']})
+    tags = "".join(request.data['tags'])
+    games_list = discovery.query(
+        collection_id=settings.DISCOVERY_COLLECTION_ID,
+        environment_id=settings.DISCOVERY_ENVIRONMENT_ID,
+        natural_language_query=tags)['results']
+    results = []
+    for game in games_list:
+        results.append({"name": game['name'], "summary": game['summary'], "cover_url": game['cover']['url'],
+                        "slug": game['slug']})
 
-        return Response(results)
+    return Response(results)
 
 
 @api_view(['POST'])
 def game_detail(request):
     if request.method == 'POST':
-        id = request.data['slug']
+        slug = request.data['slug']
         game = discovery.query(
             collection_id=settings.DISCOVERY_COLLECTION_ID,
             environment_id=settings.DISCOVERY_ENVIRONMENT_ID,
-            filter="slug::" + id)['results'][0]
+            filter="slug::" + slug)['results'][0]
 
         return Response(game)
