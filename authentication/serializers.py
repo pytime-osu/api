0683102fafebcedf20a726356acf0da22620a13f
Justin Perry <justinperry2164@gmail.com>
2020-10-18 19:56:29 -0400

    Erased the custom field in the serializer, is not needed

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -7,9 +7,6 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     @classmethod
     def get_token(cls, user):
         token = super(MyTokenObtainPairSerializer, cls).get_token(user)
-
-        # Add custom claims
-        token['nickName'] = user.nickName
         return token
 
 
