b2882f7fdcce3b99db1810c7078c0f2cce412e73
Justin Perry <justinperry2164@gmail.com>
2020-10-18 19:55:33 -0400

    Finished the logic for authentication by adding a create user and login in function

--- pytime_api/urls.py
+++ pytime_api/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path
 from django.urls import path, include
-from pytime_api import views
+
 
 from core.urls import core_router
 from core.views.health import HealthCheck
