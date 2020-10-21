008d4d74d839d6383dc9719d2755a34ed088f4c3
ashwin <ashraj98@gmail.com>
2020-10-21 15:21:48 -0400

    Created script to persist GCV tagging.

--- core/models.py
+++ core/models.py
@@ -3,3 +3,9 @@ from django.db import models
 
 class Suggestion(models.Model):
     name = models.CharField(max_length=100, unique=True)
+
+
+class ImageTag(models.Model):
+    game = models.CharField(max_length=100)
+    image = models.CharField(max_length=50)
+    tag = models.CharField(max_length=50)
---
+++ core/vision.py
@@ -0,0 +1,23 @@
+from google.cloud import vision
+
+from core.models import ImageTag
+from discovery import DiscoveryClient
+
+
+def tib():
+    dc = DiscoveryClient()
+    vision_client = vision.ImageAnnotatorClient()
+    games = dc.all_documents()
+    games = games[0:5]
+    for game in games:
+        for image in game.get('screenshots', []):
+            image_id = image['image_id']
+            if ImageTag.objects.filter(image=image_id).count() == 0:
+                url = f"https://images.igdb.com/igdb/image/upload/t_1080p/{image_id}.jpg"
+                img = vision.Image()
+                img.source.image_uri = url
+                web_detection = vision_client.web_detection(image=img).web_detection
+                labels = web_detection.web_entities
+                for label in labels:
+                    ImageTag.objects.create(game=game['slug'], image_id=image_id, tag=label.description.lower())
+        print(f"Processed {game['name']}")
