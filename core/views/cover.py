13f5c49496dcc94aeaf23665a606521d548b07b8
ashwin <ashraj98@gmail.com>
2020-11-11 13:08:47 -0500

    Removed unused imports.

--- core/views/cover.py
+++ core/views/cover.py
@@ -1,6 +1,4 @@
-from rest_framework import viewsets, status, serializers
-from rest_framework.decorators import action
-from rest_framework.response import Response
+from rest_framework import viewsets, serializers
 
 from core.models import Cover
 
