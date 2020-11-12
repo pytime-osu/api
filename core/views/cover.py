c9b44ad94a48a6735737d6b38f372d51218d176b
ashwin <ashraj98@gmail.com>
2020-11-12 10:55:34 -0500

    Added new cover querying view.

--- core/views/cover.py
+++ core/views/cover.py
@@ -1,4 +1,9 @@
+from functools import reduce
+
+from django.db.models import Q, Count
 from rest_framework import viewsets, serializers
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from core.models import Cover
 
@@ -13,3 +18,12 @@ class CoverViewSet(viewsets.ModelViewSet):
     queryset = Cover.objects.all()
     serializer_class = CoverSerializer
     filterset_fields = ['game']
+
+    @action(detail=False, methods=['POST'])
+    def match(self, request):
+        games = request.data.get('games', [])
+        search = request.data.get('search', [])
+        tag_query = reduce(lambda q, t: q | Q(tag__contains=t.lower()), search, Q())
+        tags = Cover.objects.filter(game__in=games).filter(tag_query)
+        scores = tags.values('game', 'image', 'size').annotate(score=Count('tag')).order_by('-score', '-size')
+        return Response(data=scores)
